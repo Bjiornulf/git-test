3597a616d712559c850b1673cff6541089f058d9
Bjiornulf <tpeev01@gamil.com>
2021-01-27 15:35:11 +0100

    implementing eveleness in code...\n WORKING

--- helloworld.c
+++ helloworld.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+char evil[] = {72, 101, 108, 108, 111, 32, 87, 111, 114, 108, 100, 33, 10, 0}; //Evil stuff happenning...
+
 int main(void){
-    printf("Hello World!\n")
+    printf("%s", evil)
 }
